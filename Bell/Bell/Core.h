c1d9118d8ec18715bb866afd9e4d9eefd1b382ac
Damian Sirbu <sirbudamian@gmail.com>
2022-07-08 15:38:18 +0300

    console read/write functions

---
+++ Bell/Bell/Core.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+namespace bell
+{
+	namespace core
+	{
+		template<class T>
+		void print(const T& t)
+		{
+			std::cout << t;
+		}
+
+		template <class T, class ... TAIL>
+		void print(const T& t, TAIL... tail)
+		{
+			print(t);
+			print(tail...);
+		}
+
+		template<class T>
+		void println(const T& t)
+		{
+			print(t, '\n');
+		}
+
+		template<class T, class ...TAIL>
+		void println(const T& t, TAIL... tail)
+		{
+			print(t);
+			println(tail...);
+		}
+
+		template<class T>
+		T& read(const std::string& message)
+		{
+			T res;
+			print(message);
+			std::cin >> res;
+			return res;
+		}
+	}
+}
+
